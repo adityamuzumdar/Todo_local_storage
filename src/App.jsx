5652bd8745668be0c007d34fb48132202a126b37
adityamuzumdar <adityamuzumdar11@gmail.com>
2024-01-06 10:44:53 +0530

    UI done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Addtodo from './components/Addtodo'
+import Displaytodo from './components/Displaytodo'
+
+
+function App() {
+
+  return (
+
+    <div className='bg-slate-800 max-w-xl mx-auto my-10'>
+      <Addtodo/>
+      <Displaytodo/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Displaytodo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Displaytodo() {
+  return (
+    <div>
+    <div className='my-5 text-white bg-sky-900 rounded-md mx-5 p-2' >
+      test
+    </div>
+    <div className='my-5 text-white bg-sky-900 rounded-md mx-5 p-2' >
+      test
+    </div>
+    <div className='my-5 text-white bg-sky-900 rounded-md mx-5 p-2' >
+      test
+    </div>
+    </div>
+  )
+}
